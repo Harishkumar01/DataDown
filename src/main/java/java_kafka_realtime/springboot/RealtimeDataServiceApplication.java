29547af356d796c9eb886ac757cf99626e689d66
Harish Kumar <harishkumarvijayarajan@gmail.com>
2025-05-17 17:03:22 +0530

    Add files via upload

---
+++ src/main/java/java_kafka_realtime/springboot/RealtimeDataServiceApplication.java
@@ -0,0 +1,13 @@
+package java_kafka_realtime.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java_kafka_realtime/springboot/RealtimeDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package java_kafka_realtime.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
